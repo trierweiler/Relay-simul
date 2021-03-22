622cf3ef8b92eed1b52c7c1ade06966d720a1ebd
Gabriel Trierweiler Ribeiro <gabrielribeiro.ee@gmail.com>
2021-03-22 20:31:23 -0300

    Update relay.py

--- src/relay.py
+++ src/relay.py
@@ -44,7 +44,7 @@ with expander_6:
 # =============================================================================
 expander_1 = st.beta_expander(label='Diagrama unifilar')
 with expander_1:
-    image = Image.open('/src/unifilar.jpg')
+    image = Image.open('./unifilar.jpg')
     st.image(image)
 
 # =============================================================================
